314d253fa21354a0437203836688c63d97b53a1e
Wesley <weealves1@gmail.com>
2020-03-24 14:18:52 -0300

    alteração db

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@ $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 
 
-$host = $url["host"].'/';
+$host = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $database = substr($url["path"], 1);
