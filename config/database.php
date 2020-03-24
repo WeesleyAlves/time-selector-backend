dfc393a71405022979c98eb39c06980f13acff0c
Wesley <weealves1@gmail.com>
2020-03-24 14:14:51 -0300

    alteração db

--- config/database.php
+++ config/database.php
@@ -83,16 +83,24 @@ return [
             ]) : [],
         ],
 
-        'heroku' => array(
+        'heroku' => [
             'driver' => 'mysql',
             'host' => $host,
+            'port' => env('DB_PORT', '3306'),
             'database' => $database,
             'username' => $username,
             'password' => $password,
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-        ),
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
